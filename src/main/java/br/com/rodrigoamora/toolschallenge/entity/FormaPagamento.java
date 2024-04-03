3141db992b10918a1b75e133af7fa162a812b8d3
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-03 05:41:44 -0300

    Removendo imports não usados

--- src/main/java/br/com/rodrigoamora/toolschallenge/entity/FormaPagamento.java
+++ src/main/java/br/com/rodrigoamora/toolschallenge/entity/FormaPagamento.java
@@ -2,15 +2,12 @@ package br.com.rodrigoamora.toolschallenge.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
