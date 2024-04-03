f57c45156f892f46f7b7bf34e1a0560b7f4bb1f4
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-02 23:36:36 -0300

    Configurando o acesso a rota do Swagger

---
+++ src/main/java/br/com/rodrigoamora/toolschallenge/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package br.com.rodrigoamora.toolschallenge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(
+        jsr250Enabled = true
+)
+public class WebSecurityConfig {
+
+	@Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		
+		http.csrf(csrf -> csrf.disable())
+
+        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(req -> {
+        	req.requestMatchers(HttpMethod.GET, "/").permitAll();
+            req.anyRequest().authenticated();
+        });
+        
+        return http.build();
+    }
+	
+	@Bean
+	WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/ignore2", "/swagger-ui/**","/swagger-ui.html", "/v3/api-docs/**", "/v2/api-docs/**");
+	}
+	
+}
