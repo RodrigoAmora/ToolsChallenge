7f91c2a5510daf7a629070a39a42bec48b1993a2
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-04 02:18:08 -0300

    Implementando teste de realizar pagamento com o Rest Assured

--- src/test/java/br/com/rodrigoamora/toolschallenge/PagamentoApiTest.java
+++ src/test/java/br/com/rodrigoamora/toolschallenge/PagamentoApiTest.java
@@ -1,9 +1,7 @@
 package br.com.rodrigoamora.toolschallenge;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 import java.util.UUID;
 
@@ -12,9 +10,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.rodrigoamora.toolschallenge.controller.PagamentoController;
 import br.com.rodrigoamora.toolschallenge.entity.Descricao;
@@ -23,31 +22,30 @@ import br.com.rodrigoamora.toolschallenge.entity.Pagamento;
 import br.com.rodrigoamora.toolschallenge.entity.TipoFormaPagamento;
 import br.com.rodrigoamora.toolschallenge.entity.Transacao;
 import br.com.rodrigoamora.toolschallenge.service.PagamentoService;
-import br.com.rodrigoamora.toolschallenge.util.FormatadorDataHora;
+import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PagamentoController.class)
 public class PagamentoApiTest {
 
-	private String baseURL = "http://localhost:8080";
-	
 	@Autowired
 	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	@MockBean
 	private PagamentoService pagamentoService;
 	
+	
 	@Test
-    public void realizarPagamentoTest() throws Exception{
+    public void realizarPagamentoTest() throws Exception {
 		//DESCRICAO
         Descricao descricao = new Descricao();
         long codigoAutorizacao = Math.abs(UUID.randomUUID().getMostSignificantBits());
         descricao.setCodigoAutorizacao(codigoAutorizacao);
         
-        String padraoDataHora = "dd/MM/yyyy HH:mm:ss";
-		String dataHoraFormatada = FormatadorDataHora.formatarDataHora(padraoDataHora);
-        descricao.setDataHora(dataHoraFormatada);
-        
+        descricao.setEstabelecimento("Starbucks");
         descricao.setNsu("1234567890");
         descricao.setValor(10.50);
         
@@ -62,12 +60,55 @@ public class PagamentoApiTest {
         transacao.setFormaPagamento(formaPagamento);
         
         Pagamento pagamento = new Pagamento();
+        pagamento.setId(1L);
         pagamento.setTransacao(transacao);
         
-		mvc.perform(post(baseURL+"/pagamento")
-					.content(pagamento.toString())
-					.contentType(MediaType.APPLICATION_JSON))
-			      	.andExpect(status().isOk())
-			      	.andExpect(jsonPath("$[0].transacao.id", is(pagamento.getTransacao().getId())));
+        
+        String a = this.objectMapper.writeValueAsString(pagamento);
+        
+        given()
+        .contentType(ContentType.JSON)
+        .body(a)
+        .post("/pagamento")
+        .then()
+        .body("transacao.id", notNullValue())
+        .statusCode(200);
     }
+	
+//	@Test
+//    public void realizarPagamentoTestWithMockMvc() throws Exception {
+//		//DESCRICAO
+//        Descricao descricao = new Descricao();
+//        long codigoAutorizacao = Math.abs(UUID.randomUUID().getMostSignificantBits());
+//        descricao.setCodigoAutorizacao(codigoAutorizacao);
+//        
+//        descricao.setEstabelecimento("Starbucks");
+//        descricao.setNsu("1234567890");
+//        descricao.setValor(10.50);
+//        
+//        //FORMA DE PAGAMENTO
+//        FormaPagamento formaPagamento = new FormaPagamento();
+//        formaPagamento.setParcelas(1);
+//        formaPagamento.setTipo(TipoFormaPagamento.AVISTA);
+//        
+//        Transacao transacao = new Transacao();
+//        transacao.setCartao("1111222233334444");
+//        transacao.setDescricao(descricao);
+//        transacao.setFormaPagamento(formaPagamento);
+//        
+//        Pagamento pagamento = new Pagamento();
+//        pagamento.setId(1L);
+//        pagamento.setTransacao(transacao);
+//        
+//        String a = this.objectMapper.writeValueAsString(pagamento);
+//        
+//        String endpointPagamento = "/pagamento";
+//        
+//		mvc.perform(post(endpointPagamento)//.with(digest().)
+//					.contentType(MediaType.APPLICATION_JSON)
+//					.content(a))
+//			      	.andExpect(status().isOk())
+//			      	.andReturn();
+//	}
+	
 }
