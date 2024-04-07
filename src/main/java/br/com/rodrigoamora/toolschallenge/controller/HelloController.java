b79848913cc3a67f370d8c6fcb26e7a6ae614434
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-07 17:36:03 -0300

    Renomeando a classe DocController para HelloController

--- src/main/java/br/com/rodrigoamora/toolschallenge/controller/HelloController.java
+++ src/main/java/br/com/rodrigoamora/toolschallenge/controller/HelloController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DocController {
-	@GetMapping("/redoc")
-	public String redoc() {
-		return "static/redoc.html";
+public class HelloController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index.html";
 	}
+	
 }
