8f80cfe4a3c3b699f1fbc7b42f575fd2a0711046
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-11-04 00:47:19 -0300

    Usando o Docker

---
+++ docker_build_and_run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#############################################
+### Shellscript to up Docker's containers ###
+#############################################
+
+rm -rf target/
+mvn clean install -Pdocker -DskipTests
+
+
+echo -e "\n\n"
+echo -e "\033[01;32m##############\033[01;32m"
+echo -e "\033[01;32m### Docker ###\033[01;32m"
+echo -e "\033[01;32m##############\033[01;32m"
+echo -e "\n"
+
+docker_image=$(docker images rodrigoamora/rodrigo-springboot)
+
+if [[ ! -z "${docker_image}" ]]; then
+	echo -e "\033[01;32mApagando imagem que roda a aplicação....\033[01;32m"
+	echo -e "\n"
+	docker rmi -f $docker_image
+	echo -e "\n"
+fi
+
+echo -e "\n\n"
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\033[01;32m### Building containers ###\033[01;32m"
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\n\n"
+
+sudo docker-compose build
+
+echo -e "\n\n"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\033[01;32m### Uping containers ###\033[01;32m"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\n\n"
+
+sudo docker-compose up -d
