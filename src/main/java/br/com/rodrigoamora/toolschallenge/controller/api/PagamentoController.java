9fc1d1d205e9c0e9cfc642d43d939740758f59a1
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2026-06-24 00:51:11 -0300

    Usando tags do Swagger

--- src/main/java/br/com/rodrigoamora/toolschallenge/controller/api/PagamentoController.java
+++ src/main/java/br/com/rodrigoamora/toolschallenge/controller/api/PagamentoController.java
@@ -1,5 +1,10 @@
 package br.com.rodrigoamora.toolschallenge.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -20,19 +25,31 @@ import br.com.rodrigoamora.toolschallenge.service.impl.PagamentoServiceImpl;
 public class PagamentoController {
 
 	@Autowired
-	PagamentoServiceImpl pagamentoService;
-	
+	private PagamentoServiceImpl pagamentoService;
+
+	@Operation(summary = "Realização do Pagamento")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Realização do Pagmento.", content = @Content(schema = @Schema(implementation = Pagamento.class))),
+	})
 	@PostMapping
 	public Pagamento realizarPagamento(@RequestBody Pagamento pagamento) {
 		return this.pagamentoService.realizarPagamento(pagamento);
 	}
-	
+
+	@Operation(summary = "Listar Pagamentos")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Listar todos os pagmentos.", content = @Content(schema = @Schema(implementation = Pagamento.class))),
+	})
 	@GetMapping
 	public Page<Pagamento> listarTodos(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 									   @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
 		return this.pagamentoService.listarTodos(page, size);
 	}
-	
+
+	@Operation(summary = "Listar Pagamento")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Listar pagmento pelo ID.", content = @Content(schema = @Schema(implementation = Pagamento.class))),
+	})
 	@GetMapping("/{pagamentoId}")
 	public ResponseEntity<Pagamento> buscarPagamentoPorId(@PathVariable(name = "pagamentoId") Long pagamentoId) {
 		Pagamento pagamento = this.pagamentoService.buscarPagamentoPorId(pagamentoId);
@@ -43,7 +60,11 @@ public class PagamentoController {
 		
 		return ResponseEntity.notFound().build();
 	}
-	
+
+	@Operation(summary = "Estornar do Pagamento")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Estornar pagmento pelo ID.", content = @Content(schema = @Schema(implementation = Pagamento.class))),
+	})
 	@PutMapping("/{id}/estornar")
 	public ResponseEntity<Pagamento> estornarPagamento(@PathVariable(name = "id") Long id) {
 		Pagamento pagamento = this.pagamentoService.buscarPagamentoPorId(id);
@@ -55,19 +76,31 @@ public class PagamentoController {
 		
 		return ResponseEntity.notFound().build();
 	}
-	
+
+	@Operation(summary = "Listar Pagamentos Autorizados")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Listar todos os pagamentos com status autorizado.", content = @Content(schema = @Schema(implementation = Pagamento.class))),
+	})
 	@GetMapping("/autorizado")
 	public Page<Pagamento> buscarPagamentoComStatusAutorizado(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 			   										          @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
 		return this.pagamentoService.buscarPagamentoComStatusAutorizado(page, size);
 	}
-	
+
+	@Operation(summary = "Listar Pagamentos Cancelados")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Listar todos os pagamentos com status cancelado.", content = @Content(schema = @Schema(implementation = Pagamento.class))),
+	})
 	@GetMapping("/cancelado")
 	public Page<Pagamento> buscarPagamentoComStatusCancelado(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 			   										         @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
 		return this.pagamentoService.buscarPagamentoComStatusCancelado(page, size);
 	}
 
+	@Operation(summary = "Listar Pagamentos Negados")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Listar todos os pagamentos com status nagado.", content = @Content(schema = @Schema(implementation = Pagamento.class))),
+	})
 	@GetMapping("/negado")
 	public Page<Pagamento> buscarPagamentoComStatusNegado(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 			   										      @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
